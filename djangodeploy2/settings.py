494dd91877e02afdd63cf0b50835d4ebb2c9ef70
ethan <ethanowicks9@gmail.com>
2024-02-10 21:09:55 +0000

    Add secretkey

--- djangodeploy2/settings.py
+++ djangodeploy2/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+if os.path.isfile('env.py'):
+    import env
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jq9wyee_a-yca!oz7^v#i2vga#*ou5k3t*peaaqyr)***x-179'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
---
+++ env.py
@@ -0,0 +1,2 @@
+import os
+os.environ.setdefault("SECRET_KEY", "2v5b=w90_+cdr)k+soypucv3w2a)$i9ne241*%x9v!cs+a)0a#")
