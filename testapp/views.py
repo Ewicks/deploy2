63fe149fcc66ca8cb8d022d486fc978e117729df
ethan <ethanowicks9@gmail.com>
2024-02-11 14:57:42 +0000

    s

--- testapp/views.py
+++ testapp/views.py
@@ -3,19 +3,28 @@ from bs4 import BeautifulSoup
 import requests
 import selenium
 from selenium import webdriver
+import os
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 
 def index(request):
 
+    options = Options()
+    options.add_argument('headless')
+    print('sa')
+    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    # print('s')
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('headless')
-    driver = webdriver.Chrome(options=chrome_options)
-
-
-    url = 'https://www.google.com'
-    driver.get(url)
+    driver.get('https://www.bbc.com')
+ 
     test = driver.page_source
+    print(test)
+
+    driver.close()
 
     return render(request, 'index.html', {'test': test})
