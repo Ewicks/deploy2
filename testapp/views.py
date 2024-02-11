b46f81c097d9c89a87e72bf44d23c590d89143e0
ethan <ethanowicks9@gmail.com>
2024-02-11 14:26:17 +0000

    a

--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+
 
 def index(request):
-    return render(request, 'index.html', {})
+
+    page = requests.get('http://www.google.com')
+  
+    summary_soup = BeautifulSoup(page.content, "html.parser")
+    print(summary_soup)
+
+
+    return render(request, 'index.html', {'test': summary_soup})
