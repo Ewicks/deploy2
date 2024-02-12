0ae5de3d7dd5649c420ca07bef9a7bd9896963f5
ethan <ethanowicks9@gmail.com>
2024-02-12 20:38:49 +0000

    a

--- testapp/views.py
+++ testapp/views.py
@@ -1,30 +1,25 @@
 from django.shortcuts import render
-from bs4 import BeautifulSoup
-import requests
-import selenium
-from selenium import webdriver
 import os
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
-
-
+from selenium import webdriver
 
 def index(request):
+    # Set ChromeOptions
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    
+    # Set Chrome binary location from environment variable
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-    options = Options()
-    options.add_argument('headless')
-    print('sa')
-    driver = webdriver.Chrome(options=options)
-    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    # print('s')
+    # Initialize WebDriver
+    driver = webdriver.Chrome(options=chrome_options)
 
+    # Get webpage
     driver.get('https://www.bbc.com')
- 
     test = driver.page_source
-    print(test)
 
+    # Close WebDriver
     driver.close()
 
     return render(request, 'index.html', {'test': test})
