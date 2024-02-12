2f9e3dd2e9964b783725bcd020c8b78018e6f1d4
ethan <ethanowicks9@gmail.com>
2024-02-12 20:46:26 +0000

    a

--- env.py
+++ env.py
@@ -1,2 +1,3 @@
 import os
 os.environ.setdefault("SECRET_KEY", "2v5b=w90_+cdr)k+soypucv3w2a)$i9ne241*%x9v!cs+a)0a#")
+os.environ.setdefault("GOOGLE_CHROME_BIN", "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
