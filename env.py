b31729bf119d7e4f151782e5445eebf78cc6c378
ethan <ethanowicks9@gmail.com>
2024-02-13 21:41:05 +0000

    a

--- env.py
+++ env.py
@@ -1,3 +1,7 @@
 import os
 os.environ.setdefault("SECRET_KEY", "2v5b=w90_+cdr)k+soypucv3w2a)$i9ne241*%x9v!cs+a)0a#")
 os.environ.setdefault("GOOGLE_CHROME_BIN", "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
+os.environ.setdefault("API-KEY", "LCX9HG6HVFNSR6YAACVCVSKJS1S7HHRD1Y99HC8YEJJA0JJSBJGBC8NA2UEZHH1S7PFN0CFNY0T2LCDO")
+os.environ.setdefault("DATABASE_URL", "postgres://bdstpcmg:ICndEvVIqUNouK7gj1SddU4_tnbscxXo@surus.db.elephantsql.com/bdstpcmg")
+os.environ.setdefault("CREDENTIALS_JSON_PATH", "borough-bot/credentials.json")
+
