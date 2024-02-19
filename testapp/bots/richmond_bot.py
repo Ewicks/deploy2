2cf1d750efca3885d598cf37348b40c504f93480
ethan <ethanowicks9@gmail.com>
2024-02-19 13:24:26 +0000

    Remove text

--- testapp/bots/richmond_bot.py
+++ testapp/bots/richmond_bot.py
@@ -55,7 +55,8 @@ def richmond_bot(startdate, enddate, wordlist):
     # Set up the WebDriver (you may need to provide the path to your chromedriver executable)
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
     chrome_options.add_argument("--no-sandbox")
     
     # Set Chrome binary location from environment variable
